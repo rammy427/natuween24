928f3ab313a4b78e4fc86b7ae25b00a9480c083a
rammy427 <grex4274@gmail.com>
2024-10-26 17:01:38 -0400

    Fixed jump.

--- cat.py
+++ cat.py
@@ -19,17 +19,11 @@ class Cat:
         if keys[pygame.K_d]:
             self.__rect.move_ip(self.__SPEED * dt, 0)
         
+        # Cat can only jump if it isn't midair.
         if keys[pygame.K_w] and not self.__isJumping:
             self.__isJumping = True
         
-        if self.__isJumping:
-            self.__cur_time += dt
-            v = self.__LAUNCH_SPEED - self.__GRAVITY * self.__cur_time
-            self.__rect.move_ip(0, -v)
-            if not screen_rect.contains(self.__rect):
-                self.__cur_time = 0
-                self.__isJumping = False
-        
+        self.__jump(screen_rect, dt)
         self.__rect.clamp_ip(screen_rect)
     
     def draw(self, screen: pygame.Surface) -> None:
@@ -37,3 +31,17 @@ class Cat:
 
     def getPos(self) -> pygame.Vector2:
         return self.__rect.center
+    
+    def __jump(self, screen_rect: pygame.Rect, dt: float) -> None:
+        if self.__isJumping:
+            self.__cur_time += dt
+            v = self.__LAUNCH_SPEED - self.__GRAVITY * self.__cur_time
+            self.__rect.move_ip(0, -v)
+            if self.__hasHitGround(screen_rect):
+                # Move back a unit of v so we don't go out of bounds.
+                self.__rect.move_ip(0, v)
+                self.__cur_time = 0
+                self.__isJumping = False
+
+    def __hasHitGround(self, screen_rect: pygame.Rect) -> bool:
+        return self.__rect.bottom >= screen_rect.bottom
