560f0f074765e09a961de6596d39b61eb502345e
rammy427 <grex4274@gmail.com>
2024-10-26 20:31:14 -0400

    Added damage cooldown.

--- cat.py
+++ cat.py
@@ -3,6 +3,7 @@ import pygame
 class Cat:
     def __init__(self, center: pygame.Vector2) -> None:
         self.__MAX_HP = 9
+        self.__GOD_TIME = 3
         self.__WIDTH = 96
         self.__HEIGHT = 48
         self.__SPEED = 500
@@ -11,19 +12,29 @@ class Cat:
         self.__hp = self.__MAX_HP
         self.__rect = pygame.Rect(0, 0, self.__WIDTH, self.__HEIGHT)
         self.__rect.center = center
-        self.__isJumping = False
-        self.__cur_time = 0
+        self.__is_jumping = False
+        self.__cur_jump_time = 0
+        self.__is_invincible = False
+        self.__cur_god_time = 0
 
     def update(self, screen_rect: pygame.Rect, dt: float) -> None:
+        # Process movement inputs.
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.__rect.move_ip(-self.__SPEED * dt, 0)
         if keys[pygame.K_d]:
             self.__rect.move_ip(self.__SPEED * dt, 0)
+
+        # Update god timer.
+        if self.__is_invincible:
+            self.__cur_god_time += dt
+            if self.__cur_god_time >= self.__GOD_TIME:
+                self.__is_invincible = False
+                self.__cur_god_time = 0
         
         # Cat can only jump if it isn't midair.
-        if keys[pygame.K_w] and not self.__isJumping:
-            self.__isJumping = True
+        if keys[pygame.K_w] and not self.__is_jumping:
+            self.__is_jumping = True
         
         self.__jump(screen_rect, dt)
         self.__rect.clamp_ip(screen_rect)
@@ -38,22 +49,24 @@ class Cat:
         return self.__rect
     
     def takeDamage(self) -> None:
-        self.__hp -= 1
-        print("Life: %s." % self.__hp)
+        if not self.__is_invincible:
+            self.__hp -= 1
+            self.__is_invincible = True
+            print("Life: %s." % self.__hp)
 
     def isAlive(self) -> bool:
         return self.__hp > 0
     
     def __jump(self, screen_rect: pygame.Rect, dt: float) -> None:
-        if self.__isJumping:
-            self.__cur_time += dt
-            v = self.__LAUNCH_SPEED - self.__GRAVITY * self.__cur_time
+        if self.__is_jumping:
+            self.__cur_jump_time += dt
+            v = self.__LAUNCH_SPEED - self.__GRAVITY * self.__cur_jump_time
             self.__rect.move_ip(0, -v)
             if self.__hasHitGround(screen_rect):
                 # Move back a unit of v so we don't go out of bounds.
                 self.__rect.move_ip(0, v)
-                self.__cur_time = 0
-                self.__isJumping = False
+                self.__cur_jump_time = 0
+                self.__is_jumping = False
 
     def __hasHitGround(self, screen_rect: pygame.Rect) -> bool:
         return self.__rect.bottom >= screen_rect.bottom
