02336b41159c82b7f4ba61412727dd65f90fd4c3
rammy427 <grex4274@gmail.com>
2024-10-26 22:55:14 -0400

    Enemies now spawn from both directions.

--- enemy.py
+++ enemy.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class Enemy:
     def __init__(self, screen_rect: pygame.Rect) -> None:
@@ -7,12 +8,19 @@ class Enemy:
         self.__SPEED = 300
         self.__screen_rect = screen_rect
         self.__rect = pygame.Rect((0, 0), (self.__WIDTH, self.__HEIGHT))
-        self.__rect.bottomleft = screen_rect.bottomright
-        self.__rect.left -= 50
-        self.__color = "red"
+        self.__color = pygame.Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        # Randomly choose position and direction.
+        head_right = random.choice([True, False])
+        if head_right:
+            self.__x_dir = 1
+            self.__rect.bottomright = screen_rect.bottomleft
+        else:
+            self.__x_dir = -1
+            self.__rect.bottomleft = screen_rect.bottomright
 
     def update(self, dt: float) -> None:
-        self.__rect.move_ip(-self.__SPEED * dt, 0)
+        self.__rect.move_ip(self.__x_dir * self.__SPEED * dt, 0)
         # Wrap around the screen.
         if self.__rect.right <= self.__screen_rect.left:
             self.__rect.left = self.__screen_rect.right
