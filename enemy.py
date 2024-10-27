a070082d4267523caca0116ac86ca74d12cd9c9c
rammy427 <grex4274@gmail.com>
2024-10-26 22:36:15 -0400

    Added enemy wrapping.

--- enemy.py
+++ enemy.py
@@ -4,7 +4,8 @@ class Enemy:
     def __init__(self, screen_rect: pygame.Rect) -> None:
         self.__WIDTH = 20
         self.__HEIGHT = 40
-        self.__SPEED = 100
+        self.__SPEED = 300
+        self.__screen_rect = screen_rect
         self.__rect = pygame.Rect((0, 0), (self.__WIDTH, self.__HEIGHT))
         self.__rect.bottomleft = screen_rect.bottomright
         self.__rect.left -= 50
@@ -12,6 +13,11 @@ class Enemy:
 
     def update(self, dt: float) -> None:
         self.__rect.move_ip(-self.__SPEED * dt, 0)
+        # Wrap around the screen.
+        if self.__rect.right <= self.__screen_rect.left:
+            self.__rect.left = self.__screen_rect.right
+        elif self.__rect.left >= self.__screen_rect.right:
+            self.__rect.right = self.__screen_rect.left
     
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.rect(screen, self.__color, self.__rect)
