783e96e800821c78a0b40fe89a125ab67c81689e
rammy427 <grex4274@gmail.com>
2024-10-28 21:55:21 -0400

    Added reticle sprite.

--- crosshair.py
+++ crosshair.py
@@ -2,16 +2,15 @@ import pygame
 
 class Crosshair:
     def __init__(self) -> None:
-        self.__WIDTH = 20
-        self.__HEIGHT = 20
-        self.__rect = pygame.Rect((0, 0), (self.__WIDTH, self.__HEIGHT))
+        self.__sprite = pygame.image.load("sprites/reticle.png")
+        self.__rect = self.__sprite.get_rect()
 
     def update(self, screen_rect: pygame.Rect) -> None:
         self.__rect.center = pygame.mouse.get_pos()
         self.__rect.clamp_ip(screen_rect)
 
     def draw(self, screen: pygame.Surface) -> None:
-        pygame.draw.rect(screen, 'white', self.__rect)
+        screen.blit(self.__sprite, self.__rect)
 
     def getPos(self) -> pygame.Vector2:
         return self.__rect.center
