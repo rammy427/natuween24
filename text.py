b1ea85572df9f342e1f67732eb44ac3b6741f64c
rammy427 <grex4274@gmail.com>
2024-10-28 22:19:26 -0400

    Changed score text placement.

--- text.py
+++ text.py
@@ -8,11 +8,11 @@ class TextManager:
     def drawScore(self, score: int, top_score: int, screen: pygame.Surface) -> None:
         score_text = self.font.render("Score: %s" % score, True, "white")
         score_rect = score_text.get_rect()
-        score_rect.topleft = self.__screen_rect.topleft
+        score_rect.topright = self.__screen_rect.topright
 
         top_score_text = self.font.render("Top Score: %s" % top_score, True, "white")
         top_score_rect = top_score_text.get_rect()
-        top_score_rect.topleft = score_rect.bottomleft
+        top_score_rect.topright = score_rect.bottomright
 
         screen.blit(score_text, score_rect)
         screen.blit(top_score_text, top_score_rect)
