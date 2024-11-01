37fd7b7073deeb83cabdd46da42b1c6e93aadbf9
rammy427 <grex4274@gmail.com>
2024-10-31 23:53:18 -0400

    Added web support.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import game as g
 import pygame
+import asyncio
 
 # Initialize window.
 pygame.init()
@@ -22,22 +23,27 @@ locked = True
 pygame.event.set_grab(True)
 pygame.mouse.set_visible(False)
 
-# Main game loop.
-while running:
-    # Poll for events. Iterate through every event in the queue.
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                locked = not locked
-                pygame.event.set_grab(locked)
-                pygame.mouse.set_visible(not locked)
-            elif event.key == pygame.K_SPACE:
-                game.doCatJump()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            game.spawn_bullet()
+async def main():
+    # Main game loop.
+    global running, locked
+    while running:
+        # Poll for events. Iterate through every event in the queue.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    locked = not locked
+                    pygame.event.set_grab(locked)
+                    pygame.mouse.set_visible(not locked)
+                elif event.key == pygame.K_SPACE:
+                    game.doCatJump()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                game.spawn_bullet()
 
-    game.run()
+        game.run()
+        await asyncio.sleep(0)
 
-pygame.quit()
+    pygame.quit()
+
+asyncio.run(main())
